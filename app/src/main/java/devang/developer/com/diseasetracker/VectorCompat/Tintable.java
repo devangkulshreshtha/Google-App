f3e832881d081a44c9fbbce1a654c0c6e4dd8d86
devangkulshreshtha <devang.kulshreshtha.cse14@itbhu.ac.in>
2015-07-07 22:52:28 +0530

    removing bugs

---
+++ app/src/main/java/devang/developer/com/diseasetracker/VectorCompat/Tintable.java
@@ -0,0 +1,10 @@
+package devang.developer.com.diseasetracker.VectorCompat;
+
+import android.content.res.ColorStateList;
+import android.graphics.PorterDuff;
+
+public interface Tintable {
+    public void setTintMode(PorterDuff.Mode tintMode);
+
+    public void setTintList(ColorStateList tint);
+}
